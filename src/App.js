21db7593fbee1ce1a9f160cf06c2ad3bd504fd12
Shifnas <shifnas@ymail.com>
2023-12-14 11:39:17 +0530

    mis a jour app + route

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/service" element={<Service />} />
       </Routes>
