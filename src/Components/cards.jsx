5bb9d097de4eddf7079c267f9a08032bfbfbfb8f
Shifnas <shifnas@ymail.com>
2023-12-19 21:53:50 +0530

    mis a jour le footer

--- src/Components/cards.jsx
+++ src/Components/cards.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { portfolioData } from "../portfolioData";
 
+// Props permet de reutiliser les codes dans plusiers composents
+// je recupere id, images, name etc.. de data, puis j'utilise card composent directement dans portefolio dans une dive 
+
 export default function Cards({ id, image, name, alt,language,link }) {
   const [modal, setModal] = useState(false);
   const toggleModal = () => {
@@ -8,7 +11,9 @@ export default function Cards({ id, image, name, alt,language,link }) {
   };
 
   return (
+
     <div>
+    
       <h2> {name} </h2>
       <img onClick={toggleModal} className="open-modal" src={image} />
       <p className="open-mdl-text" ></p>
--- src/Components/pages/Contact.jsx
+++ src/Components/pages/Contact.jsx
@@ -6,6 +6,7 @@ export default function contact() {
   return (
     <div>
       <ContactForm />
+ 
    
     </div>
   );
